4bd5b75c9f987546a342aff31bb7a85fdf843848
Aidan Matchette <aidan.j.matchette@gmail.com>
2023-03-27 07:58:28 -0700

    initial github commit

---
+++ revuebase/src/main/java/com/revuebase/revuebase/RevuebaseApplication.java
@@ -0,0 +1,13 @@
+package com.revuebase.revuebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevuebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevuebaseApplication.class, args);
+	}
+
+}
---
+++ revuebase/src/test/java/com/revuebase/revuebase/RevuebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revuebase.revuebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevuebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
